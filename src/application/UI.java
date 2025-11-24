a2e9094f50a954ab301645a738ed4bafbf54d361
cauan-monteiro <cauansevero03@gmail.com>
2025-11-24 17:01:50 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -7,21 +7,21 @@ import java.util.ArrayList;
 public class UI {
 
     public static void printBoard(ChessPiece[][] pieces) {
-        for (int i = 0; i<pieces.length; i++) {
-            IO.print((8-i) + " ");
-            for (int j=0; j<pieces[i].length; j++) {
+        for (int i = 0; i < pieces.length; i++) {
+            System.out.print((8 - i) + " ");
+            for (int j = 0; j < pieces.length; j++) {
                 printPiece(pieces[i][j]);
             }
             System.out.println();
         }
-        System.out.println("  a b c d e f g h ");
+        System.out.println("  a b c d e f g h");
     }
 
     private static void printPiece(ChessPiece piece) {
         if (piece == null) {
             IO.print("-");
         } else {
-            printPiece(piece);
+            System.out.print(piece);
         }
         IO.print(" ");
     }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -7,6 +7,7 @@ public class Position {
 
     public Position(int row, int col) {
         this.row = row;
+        this.col = col;
     }
 
     public int getRow() {
