dbd300eb964a5950be84a640c33395b4f50ada28
cauan-monteiro <cauansevero03@gmail.com>
2025-11-24 15:46:18 -0300

    Class Position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,8 @@
+import boardgame.Position;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    System.out.println("OK!");
+    Position pos = new Position(3, 5);
+    IO.println(pos);
 }
---
+++ src/boardgame/Position.java
@@ -0,0 +1,32 @@
+package boardgame;
+
+public class Position {
+    private int row;
+    private int col;
+    public Position() {}
+
+    public Position(int row, int col) {
+        this.row = row;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" + "row=" + row + ", col=" + col + '}';
+    }
+}
